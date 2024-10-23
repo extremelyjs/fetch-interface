c37a860d041b80b45f18be085c8f52f1613e539b
liuyuyang <2281487673@qq.com>
2024-10-23 21:34:58 +0800

    feat: 更改打包导出成员

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { createFactory } from "./createFactory";
-import { Method, Options, BaseOptions } from "./types";
 
 export {
     createFactory,
-    Method,
-    Options,
-    BaseOptions,
 }
